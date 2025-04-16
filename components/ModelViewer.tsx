a2b6b591e8cc967f4ba0cde0bc92aba19f374cf6
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-04-16 16:14:15 +0200

    Update ModelViewer.tsx

--- components/ModelViewer.tsx
+++ components/ModelViewer.tsx
@@ -1,45 +1,53 @@
-"use client";
+"use client"
 
-import { OrbitControls, useGLTF } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { useEffect } from "react";
+import { OrbitControls, useGLTF } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+import { useEffect } from "react"
 
 export default function ModelViewer() {
-	useEffect(() => {
-		// preload on the client side
-		useGLTF.preload("/moon20.glb");
-	}, []);
+  useEffect(() => {
+    // preload on the client side
+    useGLTF.preload("/moon20.glb")
+  }, [])
 
-	const { scene } = useGLTF("/moon20.glb");
+  const { scene } = useGLTF("/moon20.glb")
 
-	return (
-		<div className="h-screen w-full">
-			<Canvas
-				shadows
-				camera={{ position: [0, 0, 0], fov: 50 }}
-				className="bg-black"
-			>
-				<ambientLight intensity={0.5} />
-				<spotLight
-					position={[10, 10, 10]}
-					angle={0.15}
-					penumbra={1}
-					intensity={1}
-					castShadow
-				/>
-				<pointLight position={[-10, -10, -10]} intensity={0.5} />
-				<primitive object={scene} scale={1.5} position={[0, -1, 0]} />
-				<OrbitControls
-					enablePan={false}
-					enableDamping
-					dampingFactor={0.05}
-					rotateSpeed={0.5}
-					minDistance={3}
-					maxDistance={10}
-					autoRotate
-					autoRotateSpeed={0.5}
-				/>
-			</Canvas>
-		</div>
-	);
+  return (
+    <div className="h-screen w-full">
+      <Canvas
+        shadows
+        camera={{ position: [0, 0, 5], fov: 50 }}
+        className="bg-white"
+        gl={{
+          powerPreference: "default",
+          antialias: true
+        }}
+      >
+        <ambientLight intensity={1.2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          intensity={1.5}
+          castShadow
+        />
+        <pointLight position={[-10, -10, -10]} intensity={1.0} />
+        <pointLight position={[0, 0, 5]} intensity={0.8} />
+        <primitive object={scene} scale={1.5} position={[0, 0, 0]} />
+        <OrbitControls
+          enableRotate={true}
+          enablePan={false}
+          enableDamping
+          dampingFactor={0.05}
+          rotateSpeed={0.7}
+          minDistance={2}
+          maxDistance={15}
+          autoRotate
+          autoRotateSpeed={0.5}
+          enableZoom={true}
+          zoomSpeed={1.0}
+        />
+      </Canvas>
+    </div>
+  )
 }

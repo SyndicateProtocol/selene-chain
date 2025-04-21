e08675c12c9387231c9812a171caca31c401cc70
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-04-21 15:08:26 +0200

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,47 +1,6 @@
 "use client"
 
-const lunarPhases = [
-  {
-    name: "New Moon",
-    symbol: "A",
-    priority: "Prioritizes low calldata size"
-  },
-  {
-    name: "Waxing Crescent Moon",
-    symbol: "D",
-    priority: "Prioritizes contract deployments"
-  },
-  {
-    name: "First Quarter Moon",
-    symbol: "G",
-    priority: "FIFO"
-  },
-  {
-    name: "Waxing Gibbous Moon",
-    symbol: "J",
-    priority: "Prioritizes batch transactions"
-  },
-  {
-    name: "Full Moon",
-    symbol: "N",
-    priority: "Prioritizes transactions interacting with token contracts"
-  },
-  {
-    name: "Waning Gibbous Moon",
-    symbol: "R",
-    priority: "Prioritizes higher gas limits"
-  },
-  {
-    name: "Last Quarter Moon",
-    symbol: "U",
-    priority: "Prioritizes gas-efficient transactions"
-  },
-  {
-    name: "Waning Crescent Moon",
-    symbol: "X",
-    priority: "Prioritizes gas-efficient transactions"
-  }
-]
+import { lunarPhases } from "@/lib/constants"
 
 export default function Guide() {
   // TODO: update after call to lunar phase contract

b5fb21a3a31ac948bac6e46acee17dd7931e1dac
caleb <caleb@syndicate.io>
2025-04-25 09:17:21 -0500

    up

--- contracts/src/MoonphaseCalldataPermissionModule.sol
+++ contracts/src/MoonphaseCalldataPermissionModule.sol
@@ -3,11 +3,12 @@ pragma solidity 0.8.25;
 
 import {Moonphase} from "./Moonphase.sol";
 import {ICalldataPermissionModule} from "./interfaces/ICalldataPermissionModule.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MoonphaseCalldataPermissionModule is ICalldataPermissionModule {
+contract MoonphaseCalldataPermissionModule is ICalldataPermissionModule, Ownable {
     Moonphase private immutable moonphase;
 
-    constructor(Moonphase _moonphase) {
+    constructor(Moonphase _moonphase) Ownable(msg.sender) {
         moonphase = _moonphase;
     }
 

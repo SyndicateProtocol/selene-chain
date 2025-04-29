83c745bdaf222b1e2520f8fdfd08f9b5fbca8909
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-04-29 13:08:43 +0200

    Update MoonInteraction.sol

--- contracts/src/MoonInteraction.sol
+++ contracts/src/MoonInteraction.sol
@@ -5,12 +5,17 @@ import {RLPTxBreakdown} from "./RLP/RLPTxBreakdown.sol";
 import {ICalldataPermissionModule} from "./interfaces/ICalldataPermissionModule.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {Angel721} from "./Angel721.sol";
+import {MoonphaseCalldataPermissionModule} from "./MoonphaseCalldataPermissionModule.sol";
 
 contract MoonInteraction {
     Angel721 public angel;
+    MoonphaseCalldataPermissionModule public moonPhaseModule;
+    bytes4 private constant FLY_ME_TO_THE_MOON_SELECTOR =
+        bytes4(keccak256("flyMeToTheMoon()"));
 
-    constructor(address _angel721Address) {
+    constructor(address _angel721Address, address _moonPhaseModule) {
         angel = Angel721(_angel721Address);
+        moonPhaseModule = MoonphaseCalldataPermissionModule(_moonPhaseModule);
     }
 
     // TODO: @caleb [DELTA-7296]
@@ -24,8 +29,12 @@ contract MoonInteraction {
         angel.mint(to);
     }
 
+    function flyMeToTheMoon() public pure returns (string memory) {
+        return "fly me to the moon";
+    }
+
     // TODO: @kris10 [DELTA-7297]
-    // function waxingGibbous() public {}
+    function waxingGibbous() public onlyAllowedDuringPhase {}
 
     // TODO: @caleb [DELTA-7293]
     // function fullMoon() public {}
@@ -41,4 +50,31 @@ contract MoonInteraction {
 
     // TODO: @caleb [DELTA-7295]
     // function waningCrescent() public {}
+
+    function isWaxingGibbous() internal view returns (bool) {
+        string memory phase = moonPhaseModule.currentPhase();
+        return
+            keccak256(abi.encodePacked(phase)) ==
+            keccak256(abi.encodePacked("Waxing Gibbous"));
+    }
+
+    modifier onlyAllowedDuringPhase() {
+        if (isWaxingGibbous()) {
+            require(
+                msg.sig == FLY_ME_TO_THE_MOON_SELECTOR,
+                "Only flyMeToTheMoon during Waxing Gibbous"
+            );
+        }
+        _;
+    }
+
+    fallback() external {
+        if (isWaxingGibbous()) {
+            require(
+                msg.sig == FLY_ME_TO_THE_MOON_SELECTOR,
+                "Only flyMeToTheMoon during Waxing Gibbous"
+            );
+        }
+        revert("Function not found");
+    }
 }

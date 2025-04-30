6c2de323493a6964d6ebddfa6b991f2c67a50ea1
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-04-30 17:30:38 +0200

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,42 +6,42 @@ export interface LunarPhase {
 
 export const lunarPhases: LunarPhase[] = [
   {
-    name: "New Moon",
+    name: "New",
     symbol: "A",
     priority: "Prioritizes low calldata size"
   },
   {
-    name: "Waxing Crescent Moon",
+    name: "Waxing Crescent",
     symbol: "D",
     priority: "Prioritizes contract deployments"
   },
   {
-    name: "First Quarter Moon",
+    name: "First Quarter",
     symbol: "G",
     priority: "FIFO"
   },
   {
-    name: "Waxing Gibbous Moon",
+    name: "Waxing Gibbous",
     symbol: "J",
     priority: "Prioritizes batch transactions"
   },
   {
-    name: "Full Moon",
+    name: "Full",
     symbol: "N",
     priority: "Prioritizes transactions interacting with token contracts"
   },
   {
-    name: "Waning Gibbous Moon",
+    name: "Waning Gibbous",
     symbol: "R",
     priority: "Prioritizes higher gas limits"
   },
   {
-    name: "Last Quarter Moon",
+    name: "Last Quarter",
     symbol: "U",
     priority: "Prioritizes gas-efficient transactions"
   },
   {
-    name: "Waning Crescent Moon",
+    name: "Waning Crescent",
     symbol: "X",
     priority: "Prioritizes gas-efficient transactions"
   }

9fa39c03e5ca0b71d00a085d2b045c4d5f01182a
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-05-05 12:16:40 +0200

    sort phases correctly

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import type { LunarPhaseInterface } from "./constants"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function reorderLunarPhases(
+  phases: LunarPhaseInterface[],
+  firstPhaseName: string
+): LunarPhaseInterface[] {
+  const index = phases.findIndex((phase) => phase.name === firstPhaseName)
+  if (index === -1) {
+    return phases
+  }
+  const firstPart = phases.slice(index)
+  const secondPart = phases.slice(0, index)
+  return firstPart.concat(secondPart)
 }

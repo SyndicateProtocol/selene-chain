4bccd4b712b1fbc80da06c1f607fb5bc94a611f4
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-05-06 19:36:56 +0200

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -9,8 +9,6 @@ import { reorderLunarPhases } from "@/lib/utils";
 export default function Guide() {
 
   const moonPhase = useMoonPhase();
-
-
   const sortedPhases = reorderLunarPhases(lunarPhases, moonPhase);
 
   return (
@@ -18,7 +16,7 @@ export default function Guide() {
       {sortedPhases.map((phase) => (
         <div
           key={phase.name}
-          className={`px-2 py-2  flex items-center gap-4  rounded-xl border text-black backdrop-blur-sm ${phase.name === moonPhase ? " !bg-black text-white border-black" : ""
+          className={`px-2 py-2  flex items-center gap-4  rounded-xl border text-black bg-white/40 backdrop-blur-sm ${phase.name === moonPhase ? " !bg-black text-white border-black" : ""
             }`}
         >
           <span className="font-moonphases text-4xl">{phase.symbol}</span>

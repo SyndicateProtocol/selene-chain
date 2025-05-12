b7bb6bbab0d71731579aabe8f9a712cbdd8042a0
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-05-12 11:51:10 +0200

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,27 +1,26 @@
 "use client"
 
 import { lunarPhases } from "@/lib/constants"
-import { useMoonPhase } from "@/lib/hooks";
+import { useMoonPhase } from "@/lib/hooks"
 
-import { reorderLunarPhases } from "@/lib/utils";
-
+import { reorderLunarPhases } from "@/lib/utils"
 
 export default function Guide() {
-
-  const moonPhase = useMoonPhase();
-  const sortedPhases = reorderLunarPhases(lunarPhases, moonPhase);
+  const moonPhase = useMoonPhase()
+  const sortedPhases = reorderLunarPhases(lunarPhases, moonPhase)
 
   return (
     <div className="grid gap-2 max-w-96 min-w-72 font-geist">
       {sortedPhases.map((phase) => (
         <div
           key={phase.name}
-          className={`px-2 py-2  flex items-center gap-4  rounded-xl border text-black bg-white/40 backdrop-blur-sm ${phase.name === moonPhase ? " !bg-black text-white border-black" : ""
-            }`}
+          className={`px-2 py-2  flex items-center gap-4  rounded-xl border text-black bg-white/40 backdrop-blur-sm ${
+            phase.name === moonPhase ? " !bg-black text-white border-black" : ""
+          }`}
         >
           <span className="font-moonphases text-4xl">{phase.symbol}</span>
           <div className="leading-none">
-            <h3 className="text-lg font-medium">{phase.name} Moon</h3>
+            <h3 className="text-lg font-medium">{phase.name}</h3>
             <p className="font-mono text-xs">{phase.priority}</p>
           </div>
           {phase.name === moonPhase && (
